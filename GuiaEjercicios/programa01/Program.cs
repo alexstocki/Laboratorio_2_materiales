264986b0f3aaca4260b9d1f524cd362c1d5acfb1
Alex Stocki <alexstocki22@gmail.com>
2018-09-12 16:48:52 -0300

    Add files via upload

---
+++ GuiaEjercicios/programa01/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 01";
+
+            /* Ejercicio 01. 
+             * Solicitar el ingreso de 5 números y mostrar: 
+             * Minimo, maximo y promedio de los numeros ingresados.
+             * Agregarle color de fondo a los resultados que se imprimen por pantalla.
+             */
+
+            int numero;
+            int contador = 0;
+            int maximo = 0, minimo = 0, suma = 0;
+            float promedio = 0;
+            string lectura;
+
+            while(contador < 5)
+            {
+                Console.Write("Ingrese un numero: ");
+                lectura = Console.ReadLine();
+                if(int.TryParse(lectura, out numero))
+                {
+                    if(contador == 0)
+                    {
+                        maximo = numero;
+                        minimo = numero;
+                    }
+
+                    if(numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                    if(numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+
+                    suma += numero;
+                    contador++;
+                }
+
+                else
+                {
+                    Console.WriteLine("ERROR. Ingrese un numero valido");
+                }
+            }
+
+            promedio = suma / 5;
+
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Maximo ingresado: {0}", maximo);
+            Console.WriteLine("Minimo ingresado: {0}", minimo);
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Promedio: {0}", promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GuiaEjercicios/programa02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 02";
+
+            int valido = 0;
+            int numero = 0;
+            string lectura;
+            double cubo = 0, cuadrado = 0;
+
+            while(valido == 0)
+            {
+                Console.Write("Ingrese un numero (debe ser mayor a 0): ");
+                lectura = Console.ReadLine();
+
+                if (int.TryParse(lectura, out numero))
+                {
+                    if(numero > 0)
+                    {
+                        valido = 1;
+
+                        cuadrado = Math.Pow(numero, 2);
+                        cubo = Math.Pow(numero, 3);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("ERROR. El numero debe ser mayor a 0");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. Debe ingresar un numero valido");
+                }
+            }
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Numero: {0}", numero);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Cuadrado: {0}", cuadrado);
+            Console.WriteLine("Cubo: {0}", cubo);
+
+            Console.ReadKey();
+        }
+    }
+}
